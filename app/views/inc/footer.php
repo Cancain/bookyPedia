33034862ba66c5bca8f11503274cb9d48ffdf527
Cancain <eriksson.tomas@gmail.com>
2019-01-16 14:37:23 +0100

    set up the grid and added a basic nav-bar

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,3 +1,7 @@
+<!-- End of mainContent -->
+</div>
+<!-- End of mainGrid -->
+</div> 
 <script src="<?php echo URLROOT;?>/js/main.js"></script>
 </body>
 </html>
---
+++ app/views/inc/navBar.php
@@ -0,0 +1,14 @@
+<div id="userBar">
+    <ul class="hNav">
+        <li>Log in</li>
+        <li>Register</li>
+    </ul>
+</div>
+<div id="mainNav">
+    <nav>
+        <ul class="hNav center">
+            <li>Find book</li>
+            <li>Find author</li>
+        </ul>
+    </nav>
+</div>
