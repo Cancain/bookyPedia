2b2044f4dc053316f58f4880142ab3966398b4a4
Cancain <eriksson.tomas@gmail.com>
2019-01-16 21:50:38 +0100

    finished form validation for the register page

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -2,13 +2,91 @@
 class Users extends Controller {
 
     public function register(){
+        $title = 'Register';
+        $body = 'This is where you register an account';
 
-        $data = [
-            'title' => 'Register',
-            'body' => 'This is where you register an account on this site'
-        ];
 
-        $this->view('users/register', $data);
+        //Checking for posts
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+            //Sanitize the data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+
+            $data = [
+                'title' => $title,
+                'body' => $body,
+                'email' => trim($_POST['email']),
+                'userName' => trim($_POST['userName']),
+                'password' => trim($_POST['password']),
+                'confirmPassword' => trim($_POST['confirmPassword']),
+                'emailErr' => '',
+                'userNameErr' => '',
+                'passwordErr' => '',
+                'confirmPasswordErr' => ''
+            ];
+
+            //Validate email
+            if(empty($data['email'])){
+                $data['emailErr'] = 'You must enter a valid email';
+            } else {
+                //TODO: check for duplicate emails
+            }
+
+            //Validate the username
+            //cannot be empty
+            if(empty($data['userName'])){
+                $data['userNameErr'] = 'You must enter a user name';
+            }
+
+            //Validate password
+            //cannot be empty or less then 6 characters
+            if(empty($data['password'])){
+                $data['passwordErr'] = 'You must enter a password';
+            } elseif(strlen($data['password'] < 6)) {
+                $data['passwordErr'] = 'Your password must be atleast 6 characters long';
+            }
+
+            //Validate confirm password
+            //cannot be empty and must match the other password
+            if(empty($data['confirmPassword'])){
+                $data['confirmPasswordErr'] = "You must confirm your password";
+            } else {
+                if($data['password'] != $data['confirmPassword']){
+                    $data['confirmPasswordErr'] = 'Your passwords does not match';
+                }
+            }
+
+            //Check that all errormessages are empty
+            if(empty($data['emailErr']) && empty($data['userNameErr']) 
+            && empty($data['passwordErr']) && empty($data['confirmPasswordErr'])){
+
+                $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+                
+                die('User Added');
+            } else {
+                $this->view('users/register', $data);
+            }
+
+            
+
+        } else {
+            $data = [
+                'title' => $title,
+                'body' => $body,
+                'email' => '',
+                'userName' => '',
+                'password' => '',
+                'confirmPassword' => '',
+                'emailErr' => '',
+                'userNameErr' => '',
+                'passwordErr' => '',
+                'confirmPasswordErr' => ''
+            ];
+            $this->view('users/register', $data);
+        }
+
+        
     }
 
     public function login(){
--- app/views/users/register.php
+++ app/views/users/register.php
@@ -6,22 +6,26 @@
     <?php echo $data['body']?>
 </p>
 
-<form action="" method="post">
+<form action="<?php echo URLROOT?>/users/register" method="post">
     <div class="formWrapper">
         <label for="email">Email</label> <br>
-        <input type="email" name="email">
+        <input type="email" name="email" value="<?php echo $data['email']?>"><br>
+        <span class="error"><?php echo $data['emailErr'] ?></span>
     </div>
     <div class="formWrapper">
         <label for="userName">Username</label> <br>
-        <input type="text" name="userName">
+        <input type="text" name="userName" value="<?php echo $data['userName'] ?>"><br>
+        <span class="error"><?php echo $data['userNameErr'] ?></span>
     </div>
     <div class="formWrapper">
         <label for="password">Password</label> <br>
-        <input type="password" name="password">
+        <input type="password" name="password" value="<?php echo $data['password'] ?>"><br>
+        <span class="error"><?php echo $data['passwordErr'] ?></span>
     </div>
     <div class="formWrapper">
         <label for="ConfirmPassword">Confirm Password</label> <br>
-        <input type="password" name="confirmPassword">
+        <input type="password" name="confirmPassword" value="<?php echo $data['confirmPassword'] ?>"><br>
+        <span class="error"><?php echo $data['confirmPasswordErr'] ?></span>
     </div>
     <input type="submit" class="btn" value="Register">
 </form>
