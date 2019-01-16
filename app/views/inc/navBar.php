27b8188bad9d255bfa913ce57d1650d8fe22d2d0
Cancain <eriksson.tomas@gmail.com>
2019-01-16 16:12:36 +0100

    fixed layout for mobile navbar and searchbar

--- app/views/inc/navBar.php
+++ app/views/inc/navBar.php
@@ -1,14 +1,22 @@
+<div id="navBg"></div>
+<i id="userIcon" class="fas fa-user center"></i>
+<i id="menuIcon" class="fas fa-compass center"></i>
 <div id="userBar">
-    <ul class="hNav right">
+    <ul class="vNav center hidden">
         <li><a href="<?php echo URLROOT ?>/users/login">Log in</a></li>
         <li><a href="<?php echo URLROOT ?>/users/register">Register</a></li>
     </ul>
 </div>
+<div id="searchBar" class="center">
+    <input id="searchField" type="text" placeholder="Search" name="" id="">
+    <input id="searchBtn" class="btn" type="button" value="Go">
+</div>
 <div id="mainNav">
     <nav>
-        <ul class="hNav center">
+        <ul class="vNav center hidden">
             <li><a href="<?php echo URLROOT ?>/pages/findBook">Find books</a></li>
             <li><a href="<?php echo URLROOT ?>/pages/findAuthor">Find authors</a></li>
         </ul>
     </nav>
 </div>
+
